637aff14f435373218f21bf133197ad2f42cd167
hidayatulfatwa <fatwahidayatul@gmail.com>
2023-12-16 20:47:30 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Percobaan 9 API_F55122002_Fahruddin"
+include(":app")
